7ceadb9bb9a2f846e9b7b486060de33b1ef5d5b8
Michael Hall <github@michaelhall.ca>
2015-09-28 00:12:18 -0400

    Make the sidebar non-modifiable
    We’ve run into issues with the default side bar model, so for now we
    won’t use it.

--- other_commands.py
+++ other_commands.py
@@ -9,12 +9,3 @@ class ArcinatorKillProcessesCommand(sublime_plugin.WindowCommand):
     def run(self):
         """Runs the command"""
         thread.terminate_all()
-
-
-class ArcinatorResetSideBarCommand(sublime_plugin.WindowCommand):
-    """A command that resets the side bar to the default"""
-
-    def run(self):
-        """Runs the command"""
-        menu.remove_user_side_bar()
-        menu.create_user_side_bar()
